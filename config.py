3f7224665863ceb14aec3db7f74d3962f4d9ec0a
ftmprivate <kd3564538@gmail.com>
2024-09-14 20:29:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,13 +9,13 @@
 import os
 
 class Config:
-    API_ID = os.environ.get("API_ID", "13786499")
-    API_HASH = os.environ.get("API_HASH", "429c3423fba4cac53c02b8da777e93a8")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6659796121:AAGi_qBMIy_jYgjWcWniYtNEQr0LUMGj06w") 
+    API_ID = os.environ.get("API_ID", "28776072")
+    API_HASH = os.environ.get("API_HASH", "b3a786dce1f4e7d56674b7cadfde3c9d")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7483466812:AAH9F4vXK9db6fhW3XPY3tKpR_UFfEqfKQ0") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
     DB_URL = os.environ.get("DB_URL", "mongodb+srv://mrindianlekhak:ftm@cluster0.3ruvl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DB_NAME = os.environ.get("DB_NAME", "Cluster0")
-    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '7113960085').split()]
+    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '7042535787').split()]
 
 
 class temp(object): 
